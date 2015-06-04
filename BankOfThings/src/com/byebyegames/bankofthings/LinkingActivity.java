13a10908265bb522a362948a0bab1041dfaac021
Joseph Lee <jin3lee@gmail.com>
2015-06-03 18:17:02 -0700

    *(added) Logging works *(bug) registering broken

--- BankOfThings/src/com/byebyegames/bankofthings/LinkingActivity.java
+++ BankOfThings/src/com/byebyegames/bankofthings/LinkingActivity.java
@@ -1,29 +1,66 @@
 package com.byebyegames.bankofthings;
 
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import javax.net.ssl.HttpsURLConnection;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.byebyegames.bankofthigns.library.JSONParser;
+
 import android.support.v7.app.ActionBarActivity;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.Debug;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
 
 public class LinkingActivity extends ActionBarActivity {
 
 	Button[] button_numbers;
 
-	Button button_abc, button_del, button_next, button_skip;
+	Button button_abc, button_del, button_next;
 	EditText tv_debitCardNumber;
 	TextView tv_error;
 
+
+	//URL to get JSON Array
+	private static String url = "https://uwb.lumengaming.com/css415/rest.php?test=foo&a=register&phone=2534726639&card_number=2934982938472";
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -45,7 +82,6 @@ public class LinkingActivity extends ActionBarActivity {
 		button_numbers[9] = (Button) findViewById(R.id.button9);
 
 		button_next = (Button) findViewById(R.id.buttonnext);
-		button_skip = (Button) findViewById(R.id.buttonskip);
 		button_abc = (Button) findViewById(R.id.buttonabc);
 		button_del = (Button) findViewById(R.id.buttondel);
 
@@ -190,22 +226,108 @@ public class LinkingActivity extends ActionBarActivity {
 					editor.putString("cardNumber", tv_debitCardNumber.getText().toString());
 					editor.commit();
 					
-					Intent i = new Intent(getApplicationContext(), RegistrationSuccessActivity.class);
-					startActivity(i);
+					runChecker();
 				}
 
 			}
 
 
 		});
-		button_skip.setOnClickListener(new View.OnClickListener() {
+	}
+	
+	
+	
+	
+	private void runChecker()
+	{
+		SharedPreferences sp = getSharedPreferences("bankofdata",Context.MODE_PRIVATE);
+		 
+		JSONParse j = new JSONParse();
+		j.setup(""+sp.getString("username","n/a"), ""+sp.getString("password","n/a"));
+		Log.d("JDT","Trying " + sp.getString("username", "n/a") + " " + sp.getString("password", "n/a"));
+		j.execute();
+	}
+	
+	private class JSONParse extends AsyncTask<String, String, JSONObject> {
+		private ProgressDialog pDialog;
+		String jUsername = null;
+		String jPassword = null;
+		int isPassed;
 
-			@Override
-			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				Intent i = new Intent(getApplicationContext(), RegistrationSuccessActivity.class);
-				startActivity(i);
+		
+		protected void setup(String username, String password)
+		{
+			jUsername = username;
+			jPassword = password;
+			isPassed = -1;
+		}
+
+		@Override
+		protected void onPreExecute() {
+			super.onPreExecute();
+			pDialog = new ProgressDialog(LinkingActivity.this);
+			pDialog.setMessage("Registering ...");
+			pDialog.setIndeterminate(false);
+			pDialog.setCancelable(true);
+			pDialog.show();
+
+		}
+
+		@Override
+		protected JSONObject doInBackground(String... args) {
+
+			JSONParser jParser = new JSONParser();
+
+			String jUrl = "https://uwb.lumengaming.com/css415/rest.php?test=foo&a=register&phone=2534726639&card_number=2934982938472&username="+jUsername+"&password="+jPassword;
+
+			Log.d("JDT","step 2\n\n" + jUrl);
+			// Getting JSON from URL
+			JSONObject json = jParser.getJSONFromUrl(jUrl);
+
+			
+			Log.d("JDT","doInBackground End");
+			return json;
+		}
+		@Override
+		protected void onPostExecute(JSONObject json) {
+
+			pDialog.dismiss();
+
+			
+			
+			if(json == null)
+			{
+				Log.d("JDT","isNULLLLLL!!!");
 			}
-		});
+			else if( json.toString().contains("Success"))
+			{
+				isPassed = 1;
+
+				Log.d("JDT","onPostExecute Start " + json.toString());
+				Log.d("JDT","onPostExecute passed");
+			}
+			else
+			{
+				isPassed = 2;
+
+				Log.d("JDT","onPostExecute failed");
+			}
+
+			if(isPassed == 1)
+			{
+				Intent i = new Intent(getApplicationContext(), RegistrationSuccessActivity.class);
+				i.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+				startActivity(i);	
+			}
+			else
+			{
+				// tell user password and username don't match
+				Intent i = new Intent(getApplicationContext(), RegistrationFailedActivity.class);
+				i.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+				startActivity(i);	
+			}
+
+			Log.d("JDT","onPostExecute End");
+		}
 	}
 }
--- BankOfThings/src/com/byebyegames/bankofthings/LoginActivity.java
+++ BankOfThings/src/com/byebyegames/bankofthings/LoginActivity.java
@@ -1,10 +1,46 @@
 package com.byebyegames.bankofthings;
 
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import javax.net.ssl.HttpsURLConnection;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.byebyegames.bankofthigns.library.JSONParser;
+
 import android.support.v7.app.ActionBarActivity;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -12,12 +48,20 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+
 public class LoginActivity extends ActionBarActivity {
+	//JSON Node Names 
+	private static final String TAG_PAYLOAD = "payload";
+	private static final String TAG_MESSAGE = "message";
+	private static final String TAG_RESULT = "result";
 
 	EditText et_username;
 	EditText et_password;
 	TextView tv_error;
-	
+
+	//URL to get JSON 
+	private static String url = "http://uwb.lumengaming.com/css415/rest.php?test=1&a=login";
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -28,35 +72,26 @@ public class LoginActivity extends ActionBarActivity {
 		tv_error = (TextView) findViewById(R.id.textViewError);
 		et_username = (EditText) findViewById(R.id.editTextUsername);
 		et_password = (EditText) findViewById(R.id.editTextPassword);
-		
+
 		tv_error.setVisibility(tv_error.INVISIBLE);
-		
+
+
+		// Case: User doesn't have info on the phone so the activity goes straight to registration
 		SharedPreferences sp = getSharedPreferences("bankofdata", Context.MODE_PRIVATE);
 		if(sp.getString("username", "-1").equals("-1"))
 		{
 			Intent i = new Intent(getApplicationContext(), RegistrationActivity.class);
-            startActivity(i);
+			startActivity(i);
 		}
-		
-		
+
 		buttonLogin.setOnClickListener(new View.OnClickListener() {
 			@Override
-			public void onClick(View v) {
-				
-				if(checkUserNameAndPassword(et_username.getText().toString(), et_password.getText().toString()))
-				{
-					Intent i = new Intent(getApplicationContext(), SendActivity.class);
-					i.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
-	                startActivity(i);	
-				}
-				else
-				{
-					// tell user password and username don't match
-					tv_error.setVisibility(tv_error.VISIBLE);
-				}
+			public void onClick(View v) 
+			{
+				checkUserNameAndPassword(et_username.getText().toString(), et_password.getText().toString());
 			}
 		});
-		
+
 		buttonSignUp.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
@@ -65,9 +100,12 @@ public class LoginActivity extends ActionBarActivity {
 				startActivity(i);	
 			}
 		});
-		
-		
+
+
 	}
+
+	// HTTP 
+
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -87,16 +125,89 @@ public class LoginActivity extends ActionBarActivity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
-	
+
 	public boolean checkUserNameAndPassword(String username, String password)
 	{
 		boolean retVal = false;
-		SharedPreferences sp = getSharedPreferences("bankofdata", Context.MODE_PRIVATE);
-		if(sp.getString("username", "-1").equals(username) 
-				&& sp.getString("password", "-1").equals(password))
+		//		SharedPreferences sp = getSharedPreferences("bankofdata", Context.MODE_PRIVATE);
+		//		if(sp.getString("username", "-1").equals(username) 
+		//				&& sp.getString("password", "-1").equals(password))
+		Log.d("JDT", "STARTING");
+		JSONParse j = new JSONParse();
+		j.setup(username, password);
+		j.execute();
+
+		Log.d("JDT", "ENDING");
+		if(j.getPassed() == 1)
 		{
 			retVal = true;
 		}
 		return retVal;
+	}
+
+	private class JSONParse extends AsyncTask<String, String, JSONObject> {
+		private ProgressDialog pDialog;
+		String jUsername = null;
+		String jPassword = null;
+		int isPassed;
+
+		protected int getPassed()
+		{
+			return isPassed;
+		}
+		protected void setup(String username, String password)
+		{
+			jUsername = username;
+			jPassword = password;
+			isPassed = -1;
+		}
+
+		@Override
+		protected void onPreExecute() {
+			super.onPreExecute();
+			pDialog = new ProgressDialog(LoginActivity.this);
+			pDialog.setMessage("Attempting to Login ...");
+			pDialog.setIndeterminate(false);
+			pDialog.setCancelable(true);
+			pDialog.show();
+
+		}
+
+		@Override
+		protected JSONObject doInBackground(String... args) {
+			JSONParser jParser = new JSONParser();
+
+			String jUrl = url + "&username=" + jUsername + "&password=" + jPassword;
+			// Getting JSON from URL
+			JSONObject json = jParser.getJSONFromUrl(jUrl);
+
+			return json;
+		}
+		@Override
+		protected void onPostExecute(JSONObject json) {
+			pDialog.dismiss();
+
+			if(json.toString().contains("\"payload\":true"))
+			{
+				isPassed = 1;
+				Log.d("JDT", "isPassed = true");
+			}
+			else
+			{
+				isPassed = 2;
+			}
+
+			if(isPassed == 1)
+			{
+				Intent i = new Intent(getApplicationContext(), SendActivity.class);
+				i.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+				startActivity(i);	
+			}
+			else
+			{
+				// tell user password and username don't match
+				tv_error.setVisibility(tv_error.VISIBLE);
+			}
+		}
 	}
 }
